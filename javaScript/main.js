d3c6f78b9921497eb7809bc9c6dd6727f1fae158
Tahany1995 <>
2021-05-30 12:05:20 +0000

    loops

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,10 +1,21 @@
 var name = prompt("Please Enter Your Name");
 console.log(name);
+while (name === '' || !isNaN(name)) {
+    name = prompt("please enter your name !!!");
+}
 alert("Hello " + name );
 document.write("<p>" + name + "</p>");
 document.write("<footer>2021 by Tahany Ali</footer>");
-
-var age = prompt(" Please Enter Your Age");
+var star = prompt("What star rating would you give, 1-5?");
+while(star < 0 || star > 5) {
+  star = prompt("please enter from 1 to 5");
+}
+for (var i = 0; i < star; i++) {
+  document.write("<img src='https://cdn.iconscout.com/icon/free/png-256/star-bookmark-favorite-shape-rank-16-28621.png' />");
+  console.log(i);
+}
+console.log("Rating is " + star);
+/*var age = prompt(" Please Enter Your Age");
 
 if(age <= 18) {
   alert("you are too young");
@@ -17,7 +28,7 @@ if(age <= 18) {
 var language = prompt("Enter Your Best programming Language");
 console.log(language);
 
-alert("You Love " + language);
+alert("You Love " + language);*/
 
 
 
