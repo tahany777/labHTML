776478b4c337fcf77d9ab5ea7e0a776e8aab613d
Tahany1995 <>
2021-05-31 11:54:39 +0000

    functions

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,34 +1,44 @@
-var name = prompt("Please Enter Your Name");
-console.log(name);
-while (name === '' || !isNaN(name)) {
-    name = prompt("please enter your name !!!");
+let name = prompt("Please Enter Your Name");
+function welcome(n1) {
+  while (n1 === '' || !isNaN(n1)) {
+    n1 = prompt("please enter your name !!!");
+  }
+  console.log(n1);
+  alert("Hello " + n1 );
+  document.write("<p>" + n1 + "</p>");
 }
-alert("Hello " + name );
-document.write("<p>" + name + "</p>");
+welcome(name);
 document.write("<footer>2021 by Tahany Ali</footer>");
-var star = prompt("What star rating would you give, 1-5?");
-while(star < 0 || star > 5) {
-  star = prompt("please enter from 1 to 5");
+let star = prompt("What star rating would you give, 1-5?");
+function stars(x) {
+  while(x < 0 || x > 5) {
+  x = prompt("please enter from 1 to 5");
+  }
+  for (var i = 0; i < x; i++) {
+    document.write("<img src='https://cdn.iconscout.com/icon/free/png-256/star-bookmark-favorite-shape-rank-16-28621.png' />");
+    console.log(i);
+  }
+  console.log("Rating is " + x);
 }
-for (var i = 0; i < star; i++) {
-  document.write("<img src='https://cdn.iconscout.com/icon/free/png-256/star-bookmark-favorite-shape-rank-16-28621.png' />");
+stars(star);
+let age = prompt(" Please Enter Your Age");
+function ageTest(y) {
+  if(y <= 18) {
+    alert("you are too young");
+  } else if (y > 18 && y < 30) {
+    alert("you are the best Candidate");
+  } else {
+    alert("God Bless You");
+  }
+}
+ageTest(age);
+let lang = prompt("Enter Your Best programming Language");
+function language(i) {
   console.log(i);
+  alert("You Love " + i);
 }
-console.log("Rating is " + star);
-var age = prompt(" Please Enter Your Age");
+language(lang);
 
-if(age <= 18) {
-  alert("you are too young");
-} else if (age > 18 && age < 30) {
-  alert("you are the best Candidate");
-} else {
-  alert("God Bless You");
-}
-
-var language = prompt("Enter Your Best programming Language");
-console.log(language);
-
-alert("You Love " + language);
 
 
 
